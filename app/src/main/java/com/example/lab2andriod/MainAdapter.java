72a1cc193663961ad467369da26b77494f6958aa
Chinmay Shah <chinmay.shah@appliedsystems.com>
2022-11-25 01:23:39 -0500

    Adapter and Raw list

---
+++ app/src/main/java/com/example/lab2andriod/MainAdapter.java
@@ -0,0 +1,60 @@
+package com.example.lab2andriod;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainAdapter extends BaseAdapter {
+    MainActivity mainActivity; ArrayList<Product> list;
+    LayoutInflater layoutInflater;
+
+    public MainAdapter(MainActivity mainActivity, ArrayList<Product> list) {
+        this.mainActivity = mainActivity;
+        this.list = list;
+        layoutInflater = (LayoutInflater) mainActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    }
+
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+
+    @Override
+    public Object getItem(int i) {
+        return list.get(i);
+    }
+
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+            View rowView = view;
+            if (rowView==null) {
+                rowView = layoutInflater.inflate(R.layout.raw_list, null, true);
+            }
+
+        TextView listname1 = rowView.findViewById(R.id.list_item1);
+        listname1.setText(list.get(i).getName());
+
+
+
+
+            return rowView;
+        }
+    }
+
---
+++ app/src/main/java/com/example/lab2andriod/Product.java
@@ -0,0 +1,38 @@
+package com.example.lab2andriod;
+
+public class Product {
+    
+    String name;
+    double price;
+    int qty;
+
+    public Product(String name, double price, int qty) {
+        this.name = name;
+        this.price = price;
+        this.qty = qty;
+    }
+
+      public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+}
