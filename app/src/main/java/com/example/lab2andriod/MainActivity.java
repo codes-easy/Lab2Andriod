006d6368cc8d1c8cca69e22597b6cad7a1f30f2f
Chinmay Shah <chinmay.shah@appliedsystems.com>
2022-11-25 01:23:39 -0500

    finalUI

--- app/src/main/java/com/example/lab2andriod/MainActivity.java
+++ app/src/main/java/com/example/lab2andriod/MainActivity.java
@@ -4,20 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ListView listView1;
-    //String listview1[] = {"Pants", "Shoes", "Hats"};
+    Button one,two,three,four,five,six,seven,eight,nine,zero,buy,clear,buybtn, managerBtn;
+    TextView Producttype, total, qtytype;
 
+    ArrayList<Product> list;
+    ListView listView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.activity_main, listview1);
-//        ListView listView = (ListView) findViewById(R.id.listview1);
-//        listView.setAdapter(adapter);
+        list = new ArrayList<>();
+        listView1 = findViewById(R.id.listview_products);
+
+
+         Product p1=new Product("Pants",20.44, 10);
+        Product p2= new Product("Shoes",10.44, 10);
+        Product p3=new Product("Hats",5.9, 10);
+
+        list.add(p1);
+        list.add(p2);
+        list.add(p3);
+        MainAdapter mainAdapter1 = new MainAdapter(MainActivity.this,list);
+        listView1.setAdapter((ListAdapter) mainAdapter1);
+
+
+
+
+
     }
 }
